ae7e4353f8a8cc3343bcf1b78979a0d65ee36e4a
SMRITI <98313893+Dextr27@users.noreply.github.com>
2023-01-22 00:02:51 +0530

    Add files via upload

---
+++ rev_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int ans[], int n) {
+    int i;
+    for(i=0;i<n; i++){
+        cout<<ans[i]<<" ";
+    }
+
+    
+}
+
+void reverseArray(int arr[],int n){
+    int ans[n];
+    for (int i = n - 1; i >= 0; i--) {
+      ans[n - i - 1] = arr[i];
+   }
+   printArray(ans, n);
+}
+
+int main() {
+    int n = 5;
+    int arr[] = {5,4,3,2,1};
+    reverseArray(arr, n);
+    return 0;
+}
